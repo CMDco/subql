dab37853aaf22aeb6da4ddf7e959428a82ba6189
Dean Edwards <dmedwards4@gmail.com>
2016-11-29 11:22:17 -0800

    feat: Parse schemas and extract query fields and mutations

--- src/sockets.js
+++ src/sockets.js
@@ -6,8 +6,11 @@ function setup(server) {
   io = require('socket.io')(server);
   io.on('connection', function (socket) {
     connected[socket.id] = socket;
-    socket.emit('init', {id: socket.id})
+    socket.emit('init', { id: socket.id })
+    io.on(socket.id, function (query) {
+      handleSubscribe(query, socket.id);
+    });
   }); 
 }
 
-module.exports = { setup }
+module.exports = { setup };
---
+++ test/sockets.js
@@ -0,0 +1,32 @@
+var socket; 
+var socketid;
+
+function subscribe(uri, query, variables = null, callback) { 
+  if (!socket) {
+    socket = io('http://localhost:4000');
+    socket.on('init', ({id}) => {
+      socketid = id;
+      console.log(id);
+    });
+  }
+  socket.on(socketid, (data) => { callback(data) });
+  socket.emit(socketid, { query });
+  // var xhr = new XMLHttpRequest();
+  // xhr.responseType = 'json';
+  // xhr.open("GET", uri, false);
+  // xhr.setRequestHeader("Content-Type", "application/json");
+  // xhr.setRequestHeader("Accept", "application/json");
+  // xhr.send(JSON.stringify({
+  //   query: query,
+  //   variables: variables
+  // }));
+  // return xhr.responseText;
+}
+
+function unsubscribe() { 
+  socket.emit('unsubscribe', { socketid });
+}
+
+subscribe(null, '{ getMessage(id: 0) { id} }', null, function (data) {
+  console.log(data);
+});
