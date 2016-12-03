d1355a7091fb4efa217638773dcc0fc2b90efba3
Martin-Ting <martinhting@gmail.com>
2016-12-02 18:29:12 -0800

    refactor: DRYd up generation of unique identifier

--- src/cabotage.js
+++ src/cabotage.js
@@ -76,19 +76,12 @@ function handleSubscribe(query, socketid) {
   const root = Object.assign({}, getRoot());
   connected[socketid].query = query.query
   
-  /**
-   * wrap resolvers with type query to 
-   */
-  // this is the only place where the query is handled 
   Object.keys(root).forEach((resolverName) => {
     if (operations[resolverName].type === 'Query') {
       let oldResolver = root[resolverName];
       root[resolverName] = function (...args) {
         let ret = oldResolver(...args);
-        let uniqKeys = otypes[operations[resolverName].value].keys;
-        let uniqIdentifier = uniqKeys.reduce((acc, curr) => {
-          return acc + curr + ret[curr];
-        }, '');
+        let uniqIdentifier = generateUniqueIdentifier(operations[resolverName].value, ret);
         db[uniqIdentifier] = !db[uniqIdentifier] ? [socketid] : [...db[uniqIdentifier], socketid];
         return ret;
       }
@@ -118,10 +111,7 @@ function triggerType(typename, obj){
   if(otypes[typename] === undefined){
     throw new Error(`triggerType :: There exists no registerd type named ${typename}`);
   }
-  let uniqKeys = otypes[typename].keys;
-  let uniqIdentifier = uniqKeys.reduce((acc, curr) => {
-    return acc + curr + obj[curr];
-  }, '');
+  let uniqIdentifier = generateUniqueIdentifier(typename, obj);
   db[uniqIdentifier].forEach((socket) => {
     if(connected[socket] !== undefined){
       db[uniqIdentifier].forEach((socketid) => {
@@ -129,6 +119,13 @@ function triggerType(typename, obj){
       });
     }
   });
+}
+
+function generateUniqueIdentifier(typename, obj){
+  let uniqKeys = otypes[typename].keys;
+  return typename + uniqKeys.reduce((acc, curr) => {
+    return acc + curr + obj[curr];
+  }, '');
 }
 
 module.exports = {
