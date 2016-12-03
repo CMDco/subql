d33c4f153d5a0322695c1baf80af726b9ef9eedc
Martin-Ting <martinhting@gmail.com>
2016-12-03 13:42:36 -0800

    feat: added unit test structure for cabotage library

--- test/test.js
+++ test/test.js
@@ -1,10 +1,58 @@
 const chai = require('chai'); 
 const expect = chai.expect;
 
-describe('unit tests', () => {
-  describe('test1', () => {
-    it('test1', done => {
+describe('Cabotage Tests', () => {
+  describe('Unit Test :: registerResolver', () => {
+    it('should throw an error when no arguments are passed in.', done => {
       expect('test1').to.equal('test1');
+      done();
+    });
+    it('should throw an error when anonymous functions are passed in', done => {
+      done();
+    });
+    it('should create keys for all functions passed in', done => {
+      done();
+    });
+  });
+
+  describe('Unit Test :: getRoot', () =>{
+    it('should return a root object', done =>{
+      done();
+    });
+  });
+
+  describe('Unit Test :: wrapResolver', () =>{
+    it('should wrap functions according to type', done => {
+      done();
+    });
+    it('should return a function that triggers the proper type', done =>{
+      done();
+    });
+  });
+
+  describe('Unit Test :: parseSchema', () => {
+    it('should throw an error when nothing is passed in', done => {
+      done();
+    });
+    it('should populate the operations object when a valid schema is passed in', done =>{
+      done();
+    });
+  });
+  describe('Unit Test :: handleDisconnect', ()=>{
+    it('should remove something from connected object', done => {
+      done();
+    });
+    it('should be refactored to another file', done => {
+      done();
+    });
+  });
+  describe('Unit Test :: triggerType', ()=>{
+    it('should trigger socket communications based on type', done => {
+      done();
+    });
+  })
+  describe('Unit Test :: generateUniqueIdentifier', ()=>{
+    it('should generate a Unique Identifier', done => {
       done();
     });
   });
